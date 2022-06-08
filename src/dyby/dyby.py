a77982eda47c0649eb74b37a34530760a598975b
Jakub Malczewski <jakub.malczewski@cern.ch>
2022-06-08 18:18:48 +0200

    adding basic db entries

--- src/dyby/dyby.py
+++ src/dyby/dyby.py
@@ -1,6 +1,20 @@
-import sys
+import sys, os
 from pathlib import Path
 import yaml
+import sqlite3
+import hashlib
+BUF_SIZE = 65536
+from collections import namedtuple
+
+emptyConfig = """
+---
+dybypath : ".dyby"
+test : "test"
+"""
+
+Record = namedtuple("Record", ["name", "hash", "path"])
+
+
 
 class FileTypeError(Exception):
     def __init__(self, *args: object) -> None:
@@ -8,34 +22,104 @@ class FileTypeError(Exception):
 
 
 class Dyby:
-    def __init__(self) -> None:
-        self.a = 1
+    def __init__(self, config) -> None:
+        self.config = config
+        self.dbPath = str(Path(config["dybypath"] + "/dyby.db").absolute())
+
+
+    def _get_hash_(self, fileName) -> int:
+        md5 = hashlib.md5()
+        with open(fileName, "rb") as file:
+            while True:
+                data = file.read(BUF_SIZE)
+                if not data:
+                    break
+                md5.update(data)
+        return str(md5.hexdigest())
+
+
+    def add(self, fileName : str, name : str = None) -> None:
+        if not name:
+            name = fileName.split("/")[-1]
+        path = str(Path(fileName).absolute())
+        fileHash = self._get_hash_(fileName)
+        if self.is_in(fileName):
+            print(f"[dyby] file: {fileName} already in database")
+            return None
+            
+        con = sqlite3.connect(self.dbPath)
+        cur = con.cursor()
+        cur.execute("insert into files values (?, ?, ?)", (name, fileHash, path))
+        con.commit()
+        con.close()        
+        print(f"[dyby] added record: {(name, fileHash, path)}")
+        #TODO add time stamp
+
+
+    def is_in(self, fileName : str) -> bool:
+        con = sqlite3.connect(self.dbPath)
+        cur = con.cursor()
+        cur.execute("SELECT COUNT(*) as count FROM files WHERE hash = (?);", (self._get_hash_(fileName), ))
+        result = cur.fetchall()[0][0]
+        con.close()
+        return result == 1
+
+
+    def count(self,) -> int:
+        con = sqlite3.connect(self.dbPath)
+        cur = con.cursor()
+        cur.execute("SELECT COUNT(*) as count FROM files;")
+        result = cur.fetchall()[0][0]
+        con.close() 
+        return result
 
 
 def db(path = './dyby.yaml') -> Dyby:
     config = get_config(path)
+    prepare_dyby(config["dybypath"])
+
+    return Dyby(config)
 
 
 def get_config(path : str) -> dict:
     configPath = Path(path)
     if configPath.exists():
         if configPath.is_dir():
-            return get_config(str(path + "/dyby.yaml"))
+            return get_config(configPath.joinpath(Path("dyby.yaml")))
         configPathStr = str(configPath)
         if configPathStr[-4:] == "yaml" or configPathStr[-3:] == "yml":
-            return yaml.load(configPathStr, yaml.SafeLoader)
+            with open(configPathStr, 'r') as file:
+                return yaml.load(file, yaml.SafeLoader)
         else:
             raise FileTypeError(f"{path} is not a yaml config file.")
     else:
-        create_config(configPath)
+        create_config(str(configPath))
         return get_config(str(configPath))
 
 
-def create_config(path : Path) -> None:
+def create_config(path : str) -> None:
     with open(path, 'w') as file:
-        yaml.dump({"test": 1}, file, yaml.SafeDumper)
+        file.write(emptyConfig)
 
 
-
+def prepare_dyby(path : str) -> None:
+    path = Path(path)
+    if not path.exists():
+        os.mkdir(path)
+    elif not path.is_dir():
+        raise NotADirectoryError(path) 
+    dbpath = Path(path).joinpath("dyby.db").absolute()
+    if not dbpath.exists():
+        create_db(dbpath)
+    else:
+        con = sqlite3.connect(dbpath)
+        con.close()
     
-    
+
+def create_db(path : str) -> None:
+    con = sqlite3.connect(path)
+    cur = con.cursor()
+    cur.execute('''CREATE TABLE files
+               (name, hash, path)''')
+    con.commit()
+    con.close()
--- tests/basic_test.py
+++ tests/basic_test.py
@@ -10,6 +10,8 @@ if path_dyby not in sys.path:
 import dyby
 import tempfile
 import pytest
+import sqlite3
+
 
 def test_config_is_folder():
     with tempfile.TemporaryDirectory() as tempDirName:
@@ -20,6 +22,7 @@ def test_config_is_folder():
 def test_config_is_not_yaml():
     from dyby import FileTypeError
     with tempfile.TemporaryDirectory() as tempDir:
+        os.chdir(tempDir)
         filePath = tempDir + "/temp.yam"
         with open(filePath, "w") as tempFile:
             tempFile.write("42\n")
@@ -38,8 +41,58 @@ def test_create_config():
         dbConfig = dyby.get_config("dyby.yaml")  
         assert Path("./dyby.yaml").exists()
         
-        config = yaml.load("dyby.yaml", yaml.SafeLoader)
-        assert config == dbConfig
+        with open("dyby.yaml") as file:
+            config = yaml.load(file, yaml.SafeLoader)
+        assert type(config) == dict
+        assert type(config["dybypath"] == str )
+
+
+def test_prepare_dyby():
+    with tempfile.TemporaryDirectory() as tempDir:
+        os.chdir(tempDir)
+        dyby.prepare_dyby("dyby")
+        assert Path("dyby").is_dir()
+        con = sqlite3.connect("dyby/dyby.db")
+        cur = con.cursor()
+        cur.execute("INSERT INTO files VALUES ('test',1548581324584,'/test')")
+        assert len(list(cur.execute('SELECT * FROM files'))) == 1
+        con.close()
+
+
+def test_Dyby_content():
+    with tempfile.TemporaryDirectory() as tempDir:
+        os.chdir(tempDir)
+        db = dyby.db()
+        assert Path(db.config["dybypath"]).is_dir()
+
+
+def test_add_get_file():
+    with tempfile.TemporaryDirectory() as tempDir:
+        os.chdir(tempDir)
+        db = dyby.db()
+        with open("testfile1.txt", "w") as file:
+            file.write("42")
+        with open("testfile2.txt", "w") as file:
+            file.write("84")
+        db.add("testfile1.txt")
+        assert db.count() == 1
+        db.add("testfile1.txt")
+        assert db.count() == 1
+        db.add("testfile1.txt", name = "testfile")
+        assert db.count() == 1
+        db.add("testfile2.txt")
+        assert db.count() == 2
+        with open("testfile1.txt", "a") as file:
+            file.write("42")
+        db.add("testfile1.txt")
+        assert db.count() == 3
+        
+        record = db.get(name = "testfile")
+        assert type(record) == dyby.Record
+        assert record.name == "testfile"
+        assert type(record.hash) == str
+        assert type(record.path) == str
+        assert Path(type(record.path)).is_file()
 
 
 if __name__ == "__main__":
